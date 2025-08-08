fc70ebbbc3dc78834656ac6d4581c0c241f1a348
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-08 23:12:14 +0700

    Refactor layout and styles in App component; add new Card component design; remove unused assets; update CSS variables.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import Card from "./component/card";
 
 export default function App() {
   return (
-    <div className="bg-plum min-h-screen text-linen">
-      <h1 className="flex items-center justify-center text-4xl font-bold">Animedl</h1>
-      <Card />
+    <div className="min-h-screen">
+      <h1 className="flex items-center justify-center text-4xl font-bold my-5">Animedl</h1>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-4 gap-4 max-w-6xl">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }
--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,7 +1,23 @@
-export default function Card() {
-    return (
-        <div className="p-3 m-10 bg-abyss shadow-sm shadow-amber-300">
-            <p className="text-xl">Halo</p>
+import React from 'react';
+
+
+const Card = () => {
+  return (
+    <a className="group relative max-w-sm">
+      <div className="absolute -z-1 -inset-1 rounded-xl bg-gradient-to-r from-teal-500 via-gold to-green-500 opacity-20 blur-xl transition-all duration-500 group-hover:opacity-50 group-hover:blur-2xl" />
+      <div className="relative x-3 py-2 rounded-xl border border-slate-800 bg-slate-950 p-2">
+        <div className="rounded-lg bg-slate-800/50">
+          <div className='px-2 py-1'>
+            <p className="text-xl font-semibold">Judul</p>
+            <p className="text-sm">Episode | Tanggal</p>
+          </div>
         </div>
-    )
-} 
+        <img className="rounded-lg mt-2" src="/img.jpg" />
+        <div className="absolute inset-x-0 -bottom-px h-px bg-gradient-to-r from-transparent via-gold to-transparent opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+        <div className="absolute inset-x-0 -top-px h-px bg-gradient-to-r from-transparent via-gold to-transparent opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+      </div>
+    </a>
+  );
+}
+
+export default Card;
