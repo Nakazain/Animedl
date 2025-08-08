1f560240463788d05d200f16cd4480c41645c756
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-08 21:59:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./component/card";
+
+export default function App() {
+  return (
+    <div className="bg-plum min-h-screen text-linen">
+      <h1 className="flex items-center justify-center text-4xl font-bold">Animedl</h1>
+      <Card />
+    </div>
+  );
+}
---
+++ src/component/card.jsx
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+        <div className="p-3 m-10 bg-abyss shadow-sm shadow-amber-300">
+            <p className="text-xl">Halo</p>
+        </div>
+    )
+} 
