37433b003b53ca2c42f526d0a34f9def75979758
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-09 21:00:05 +0700

    Little bit refactore

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import Card from "./component/card";
-import Search from "./component/search";
-import  Container from "./component/container";
+import Home from "./pages/home";
 
 export default function App() {
   return (
-    <div className="min-h-screen">
-      <h1 className="flex items-center justify-center text-4xl font-bold my-5">Animedl</h1>
-      <div className="flex justify-center">
-        <div>
-          <div className="flex justify-center mb-5">
-            <Search />
-          </div>
-          <Container title="Anime Terbaru">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </Container>
-          <Container title="Anime Tamat">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-          </Container>
-        </div>
-      </div>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
